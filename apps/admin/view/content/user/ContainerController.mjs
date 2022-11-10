2d4f366428937011761acc50c1c9bd370601905b
tobias.uhlig <tobias.uhlig@accenture.com>
2022-11-10 17:09:01 +0100

    Admin.view.ViewportController: onSwitchThemeButtonClick() => applying the theme change directly to the document.body to honor dialogs as well.

--- apps/admin/view/content/user/ContainerController.mjs
+++ apps/admin/view/content/user/ContainerController.mjs
@@ -5,6 +5,11 @@ import Component from '../../../../../node_modules/neo.mjs/src/controller/Compon
  * @extends Neo.controller.Component
  */
 class ContainerController extends Component {
+    /**
+     * @member {Admin.view.content.user.Dialog|null} dialog=null
+     */
+    dialog = null
+
     static getConfig() {return {
         /**
          * @member {String} className='Admin.view.content.user.ContainerController'
@@ -17,7 +22,23 @@ class ContainerController extends Component {
      * @param {Object} data
      */
     onCreateUserButtonClick(data) {
-        console.log('onCreateUserButtonClick', data);
+        let me     = this,
+            button = data.component;
+
+        if (!me.dialog) {
+            import('./Dialog.mjs').then(module => {
+                me.dialog = Neo.create({
+                    module         : module.default,
+                    animateTargetId: button.id,
+                    appName        : button.appName,
+                    closeAction    : 'hide',
+                    height         : 400,
+                    width          : 300
+                });
+            });
+        } else {
+            me.dialog.show();
+        }
     }
 }
 
